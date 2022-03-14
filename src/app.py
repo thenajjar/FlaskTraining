b70f269523c1d39399c498babd71796a7d998a63
Abdulwahab Alnajjar <abdulwahab.alnajjar@nana.sa>
2022-03-14 17:54:37 +0300

    Changing functions to classes and made it follow restful

--- src/app.py
+++ src/app.py
@@ -1,68 +1,71 @@
 from flask import Flask, request, render_template, Response, json
+from flask_restful import Api, Resource
+from db import create_table, create_user, get_user
+
 
 app = Flask(__name__, template_folder='templates')
-
-users_list = []
+api = Api(app)
 
 @app.route("/")
 def my_form():
-    ''' Returns a jinja template object from an html template
+    ''' Loads an html file template using jinja
     '''
     return render_template('register.html.jinja')
 
-@app.route('/register', methods=['POST'])
-def my_form_post():
-    ''' Registers new user into users_list from a post request
+@app.route("/profile")
+def user_profile():
+    ''' Loads an html file template using jinja
     '''
-    username = request.form['username']
-    name = request.form['name']
-    email = request.form['email']
-    phone = request.form['phone']
-    password = request.form['password']
-    user_id = str(len(users_list))
-    users_list.append({
-                "id": user_id,
-                "email": email,
-                "username": username,
-                "name": name,
-                "phone": phone,
-                "password": password
-            })
-    return Response(
-        response=json.dumps({
-            "data": {
-                "id": user_id,
-                "email": email,
-                "username": username,
-                "name": name,
-                "phone": phone
-            }
-        }),
-        status=201,
-        mimetype="application/json"
-    )
-    
-@app.route('/users/<string:user_id>/', methods=['GET'])
-def my_form_get(user_id):
-    ''' Take a userid and returns the data of that user
-    '''
-    username = users_list[int(user_id)]['username']
-    name = users_list[int(user_id)]['name']
-    email = users_list[int(user_id)]['email']
-    phone = users_list[int(user_id)]['phone']
-    return Response(
-        response=json.dumps({
-            "data": {
-                "id": user_id,
-                "email": email,
-                "username": username,
-                "name": name,
-                "phone": phone
-            }
-        }),
-        status=200,
-        mimetype="application/json"
-    )
+    return render_template('user_profile.html.jinja')
 
+
+class users_api(Resource):
+    def post(self):
+        ''' Registers new user into users database from a post request
+        '''
+        username = request.form['username']
+        name = request.form['name']
+        email = request.form['email']
+        phone = request.form['phone']
+        password = request.form['password']
+        user_id = create_user(name, username, email, phone, password)
+        return Response(
+            response=json.dumps({
+                "data": {
+                    "id": user_id,
+                    "email": email,
+                    "username": username,
+                    "name": name,
+                    "phone": phone
+                }
+            }),
+            status=201,
+            mimetype="application/json"
+        )
+
+
+class users_db_api(Resource):      
+    def get(self, user_id):
+        ''' Take a userid and returns the data of that user
+        '''
+        _, name, username, email, _, phone = get_user(user_id)
+        return Response(
+            response=json.dumps({
+                "data": {
+                    "id": user_id,
+                    "email": email,
+                    "username": username,
+                    "name": name,
+                    "phone": phone
+                }
+            }),
+            status=200,
+            mimetype="application/json"
+        )
+        
+
+api.add_resource(users_api, '/users', endpoint = 'users')
+api.add_resource(users_db_api, '/users/<string:user_id>', endpoint = 'user')
 if __name__ ==  "__main__":
+    # create_table()
     app.run(debug=True)
